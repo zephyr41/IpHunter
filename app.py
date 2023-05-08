9fed0bb272e225b45a421a3e32a6d6e55507c206
Nils <nils.jaudon@ynov.com>
2023-05-08 12:19:20 +0200

    FirstLabel

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-import tkinter
-gui = tkinter.Tk()
-# ajouter des widgets ici...
+from tkinter import *
+
+gui = Tk()
+
+label = Label(gui, text="Entrez une cible ")
+
+label.pack(side=TOP)
+
+ip = StringVar()  ## sert à définir l'ip
+
+input = Entry(gui, textvariable=ip)  ## sert à donner une zone de texte
+
+input.pack()  ## permet d'organiser les widgets
+
+ip.set("ex : 192.168.1.0/24")
+
+content = ip.get()
+
 gui.mainloop()
+
+
+## Une fois qu'on à la valeur : rajouter un button scan qui lance une fonction de scan
